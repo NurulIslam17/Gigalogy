d38817d7b7c0b73862db43e6ab8d560ba8efc3b2
NurulIslam17 <nurulcse09@gmail.com>
2025-12-13 20:58:37 +0600

    Adding commnets

--- app/Services/GmailService.php
+++ app/Services/GmailService.php
@@ -12,10 +12,10 @@ class GmailService
 
     public static function sendEmail($to, $subject, $body)
     {
-        $client = new Client();
-        $client->setAuthConfig(storage_path('app/google/credentials.json'));
-        $client->setAccessType('offline');
-        $client->setScopes(['https://www.googleapis.com/auth/gmail.send']);
+        $client = new Client();  //This client handles: Authentication,Token management,API requests
+        $client->setAuthConfig(storage_path('app/google/credentials.json'));  // Load OAuth Credentials: Client ID , Client Secret
+        $client->setAccessType('offline'); // Enables refresh tokens, 
+        $client->setScopes(['https://www.googleapis.com/auth/gmail.send']); // Grants permission to : Send emails only
 
         $client->setAccessToken(json_decode(file_get_contents(storage_path('app/google/token.json')), true));
 
@@ -28,26 +28,25 @@ class GmailService
             );
         }
 
-        $service = new Gmail($client);
+        $service = new Gmail($client); // This service is used to: Send messages , Read inbox
 
         // -----------------------------
         // Build MIME email manually
         // -----------------------------
         $rawMessageString =
-            "From: Your App <yourgmail@gmail.com>\r\n" .
             "To: <$to>\r\n" .
             "Subject: $subject\r\n" .
             "MIME-Version: 1.0\r\n" .
             "Content-Type: text/html; charset=utf-8\r\n\r\n" .
             $body;
 
-        // Encode to Base64URL
-        $rawMessage = rtrim(strtr(base64_encode($rawMessageString), '+/', '-_'), '=');
+        // Encode to Base64URL - Gmail API requires Base64 URL-safe encoding
+        $rawMessage = rtrim(strtr(base64_encode($rawMessageString), '+/', '-_'), '=');  
 
-        // Send Email
+        // Send Email -  Gmail Message Object : Wraps the encoded email into a Gmail-compatible object
         $message = new Gmail\Message();
         $message->setRaw($rawMessage);
 
-        return $service->users_messages->send('me', $message);
+        return $service->users_messages->send('me', $message); // 'me' → authenticated Gmail user
     }
 }
